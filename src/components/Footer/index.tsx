78e57c3c30eba4927ef29c5e79ad9daf2e933c7e
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2023-03-02 08:58:22 -0300

    chore: add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,47 @@
+import Link from 'next/link';
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandWhatsapp,
+  IconChevronUp,
+} from '@tabler/icons-react';
 import { Container } from './styles';
 
 const Footer = (): JSX.Element => {
   return (
     <Container>
       <section>
-        <h1>footer</h1>
+        <article>
+          <p>Ritz Beleza & Estética</p>
+
+          <ul>
+            <li>
+              <Link href="/">
+                <IconBrandFacebook className="facebook" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <IconBrandInstagram className="instagram" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <IconBrandWhatsapp className="whatsapp" />
+              </Link>
+            </li>
+          </ul>
+        </article>
+
+        <article>
+          <p>📞 (11) 96865-7222</p>
+          <div>
+            <p>Ir para o topo</p>
+            <Link href="/">
+              <IconChevronUp />
+            </Link>
+          </div>
+        </article>
       </section>
     </Container>
   );
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -14,17 +14,67 @@ export const Container = styled.footer`
     align-items: center;
     justify-content: space-between;
 
-    padding: 2.3rem 1.2rem;
+    padding: 13rem 1.2rem 10rem;
 
-    button {
-      right: 0;
-      color: white;
-      border: none;
-      border-radius: 4px;
-      padding: 1rem 2rem;
-      font-size: 1.6rem;
-      position: relative;
-      cursor: pointer;
+    > article {
+      width: 20rem;
+
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      justify-content: space-between;
+
+      p {
+        font-size: 1.2rem;
+        color: ${({ theme }) => theme.colors.background};
+        text-align: end;
+      }
+
+      ul {
+        margin-top: 1.6rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        li > a {
+          svg {
+            transition: 300ms;
+          }
+          &:hover {
+            & .whatsapp {
+              color: #25d366;
+            }
+            & .instagram {
+              color: #833ab4;
+            }
+            & .facebook {
+              color: #4267b2;
+            }
+          }
+        }
+      }
+
+      a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem;
+
+        border: 1px solid ${({ theme }) => theme.colors.background};
+        border-radius: 0.8rem;
+        svg {
+          color: ${({ theme }) => theme.colors.background};
+        }
+      }
+
+      > div {
+        margin-top: 1.6rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 70%;
+      }
     }
   }
 `;
